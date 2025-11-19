50193611216274e40cd29ff8b0a416f416dee364
Gosia Sinilo <gosia.sinilo@nology.io>
2025-11-19 16:05:29 +1100

    Day of the week challenge

---
+++ 19-11/script.js
@@ -0,0 +1,23 @@
+function getToday() {
+const d = new Date();;
+let day = d.getDay();
+
+if (day === 0) {
+    return "Sunday";        
+} else if (day === 1) {
+    return "Monday";            
+} else if (day === 2) {
+    return "Tuesday";            
+} else if (day === 3) {
+    return "Wednesday";
+} else if (day === 4) {
+    return "Thursday";
+} else if (day === 5) {
+    return "Friday";
+} else if (day === 6) {
+    return "Saturday";
+}
+}
+
+console.log(`Today is: ${getToday()}`); 
+
